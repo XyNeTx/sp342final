00cf2e4eaaf6bf35e140b7a0a155ea0b05cea614
Sitthiporn <2020511101002@live4.utcc.ac.th>
2022-04-27 15:34:14 +0700

    edit priceCalculate function

--- priceCal.js
+++ priceCal.js
@@ -2,8 +2,17 @@ function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  let total;
+    if(eventId == 1){
+        total =91.25*ticknum;
+    }else if(eventId == 2){
+        total = 79.88*ticknum;
+    }else if(eventId == 3){
+        total = 83.12*ticknum;
+    }else if(eventId == 4){
+        total = 100*ticknum;
+    }
+  return total;
 }
 
 
